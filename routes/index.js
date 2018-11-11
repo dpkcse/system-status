0dfb21627dbf6948f83b71626163817d6f5f40c4
dpkcse <dipokchakraborty@gmail.com>
2018-11-11 16:26:43 +0600

    upload Files and folder

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const si = require('systeminformation');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // promises style - new in version 3
+  si.cpu()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+  si.mem()
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+  
+  si.battery()
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
+  setInterval(function () {
+    si.networkStats().then(data => {
+      console.log(data);
+    })
+  }, 1000)
+  
+  res.render('index', { title: 'Express' });
+});
+
+// full async / await example (node >= 7.6)
+async function cpu() {
+  try {
+    const data = await si.cpu();
+    console.log(data)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+module.exports = router;
